697220e922219157be009a5cdd4a34f538881c2b
Sorin Turda <s.turda12@gmail.com>
2024-11-05 10:33:01 +0200

    second commit

---
+++ linksharing/src/main/java/com/example/linksharing/LinksharingApplication.java
@@ -0,0 +1,13 @@
+package com.example.linksharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinksharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinksharingApplication.class, args);
+	}
+
+}
---
+++ linksharing/src/test/java/com/example/linksharing/LinksharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linksharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinksharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
